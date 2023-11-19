b929e30befe3498c672b093e57dc0e4cc1282e56
Samuel Arogbonlo <47984109+samuelarogbonlo@users.noreply.github.com>
2023-11-19 19:29:16 +0100

    Update main.tf

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -12,11 +12,11 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    description = "connection form the ecs pods"
+    description = "connection form the VPC"
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   tags = {
